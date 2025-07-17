6cb8e2258b1a575cc58aff6f15ec286d97f34d46
Ailsa Martínez <aammishaddai@gmail.com>
2025-07-17 13:54:32 -0600

    instalando dependencias

---
+++ mi-proyecto/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-teal-500 text-white p-4 text-xl rounded-xl shadow-lg">
+      Probando Tailwind css
+    </div>
+  );
+}
+
+
